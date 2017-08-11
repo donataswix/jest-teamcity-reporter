d6ffcbdc993a36da38ffcaee78e24a9c665bb504
Donatas Petrauskas <donatasp@wix.com>
2017-08-11 15:44:12 +0300

    Don't fail in integration tests because it will fail whole build!

--- src/index.it.spec.js
+++ src/index.it.spec.js
@@ -8,22 +8,14 @@ describe('Jest TeamCity Reporter', () => {
     it('should report suite in a team city format', () => {
         const [
             suiteStart,
-            test1Ignored,
-            test2Started,
-            test2Finished,
-            test3Started,
-            test3Failed,
-            test3Finished,
+            testStarted,
+            testFinished,
             suiteFinished
         ] = getTestsOutput('10.1').split('\n');
 
         expect(suiteStart).toEqual(`##teamcity[testSuiteStarted name='src/example.spec.js']`);
-        expect(test1Ignored).toEqual(`##teamcity[testIgnored name='Suite A ignored test']`);
-        expect(test2Started).toEqual(`##teamcity[testStarted name='Suite A passing test']`);
-        expect(test2Finished).toMatch(/^##teamcity\[testFinished name='Suite A passing test' duration='\d+'\]/);
-        expect(test3Started).toEqual(`##teamcity[testStarted name='Suite B failing test']`);
-        expect(test3Failed).toMatch(/^##teamcity\[testFailed name='Suite B failing test' message='FAILED' details=/);
-        expect(test3Finished).toMatch(/^##teamcity\[testFinished name='Suite B failing test' duration='\d+'\]/);
+        expect(testStarted).toEqual(`##teamcity[testStarted name='Suite A passing test']`);
+        expect(testFinished).toMatch(/^##teamcity\[testFinished name='Suite A passing test' duration='\d+'\]/);
         expect(suiteFinished).toEqual(`##teamcity[testSuiteFinished name='src/example.spec.js']`);
     });
 
